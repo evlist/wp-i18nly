badba9c3df78991c27e33c47e029952f61f85339
Eric van der Vlist <vdv@dyomedea.com>
2026-03-02 11:01:24 +0000

    Script to set permalinks (needed by adminer)

---
+++ .devcontainer/sbin/bootstrap.sh.d/30-permalinks.local.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2026 Eric van der Vlist <vdv@dyomedea.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/' --hard
