166ea73c9fc79d55c01188bd7ef13440b801c39d
Eric van der Vlist <vdv@dyomedea.com>
2026-03-05 09:51:43 +0000

    Alias for phpcs

---
+++ .devcontainer/sbin/bootstrap.sh.d/10-aliases.local.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2026 Eric van der Vlist <vdv@dyomedea.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Add aliases to be used in codespace terminals
+alias phpcs="phpcs --standard=.vscode/phpcs.xml --warning-severity=1"
